62e17f3d52e4096fd624052b32d58071e5f930b7
qicaisheng <qicaisheng@qq.com>
2020-04-18 21:47:07 +0800

    add customerRepository

---
+++ src/main/java/com/qicaisheng/herokuspringdemo/demo/Customer.java
@@ -0,0 +1,26 @@
+package com.qicaisheng.herokuspringdemo.demo;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/qicaisheng/herokuspringdemo/demo/EmployeeRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.qicaisheng.herokuspringdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+    
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void testFindByLastName() {
+        Customer customer = new Customer("first", "last");
+        entityManager.persist(customer);
+
+        List<Customer> findByLastName = customerRepository.findByLastName(customer.getLastName());
+
+        assertThat(findByLastName).extracting(Customer::getLastName).containsOnly(customer.getLastName());
+    }
+}
