8ac2e5f8a043a5a0945e1b4062573b7e6e2f15eb
qicaisheng <qicaisheng@qq.com>
2020-04-19 10:42:18 +0800

    modify CustomerRepository

--- src/main/java/com/qicaisheng/herokuspringdemo/demo/CustomerRepository.java
+++ src/main/java/com/qicaisheng/herokuspringdemo/demo/CustomerRepository.java
@@ -1,14 +1,13 @@
 package com.qicaisheng.herokuspringdemo.demo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
 
-    Customer findById(long id);
 }
